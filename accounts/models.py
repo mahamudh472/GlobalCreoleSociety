92d9b8c66217527f11bb5b86213af02efa3af6fe
Md Mahmud Hasan <mahadymahamudh472@gmail.com>
2026-01-10 01:18:36 +0600

    profile name issue fixed

--- accounts/models.py
+++ accounts/models.py
@@ -40,7 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_("email address"), unique=True)
-    profile_name = models.CharField(_("profile name"), max_length=150, unique=True)
+    profile_name = models.CharField(_("profile name"), max_length=150)
 
     description = models.TextField(_("description"), blank=True)
     profile_image = models.ImageField(_("profile image"), upload_to="profile_images/", blank=True, null=True)
